a2343890b7dd5072f9a8a7c77a00986aacf26065
Dhruvangi Patel <dhruvipatel2493@yahoo.com>
2022-03-26 13:10:16 -0400

    code added

--- index.py
+++ index.py
@@ -2,5 +2,9 @@ hungry = input(Are you hungry?)
 if hungry == "yes":
    print("eat available food in fridge")
 else:
+ thirsty = input(Are you thirsty?)
+   if thirsty == "yes":
+    print("drink water or juice")
+else:
    print(" do your work ")
    
