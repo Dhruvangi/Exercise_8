88e28269e2e2a7bc88a39c9ae4c260621f620417
Dhruvangi Patel <dhruvipatel2493@yahoo.com>
2022-03-26 13:48:32 -0400

    version_added

---
+++ about1.py
@@ -0,0 +1,9 @@
+hungry = input(Are you hungry?)
+if hungry == "yes":
+   print("eat available food in fridge")
+else:
+ thirsty = input(Are you thirsty?)
+   if thirsty == "yes":
+    print("drink water or juice")
+else:
+   print(" do your work or go for walk")
